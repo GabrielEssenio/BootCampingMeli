06e0f83103455906922852bf51976cd6b578cd5c
Gabriel Essenio Dias Castro <gessenio@br0c02g99rbmd6m.ml.com>
2022-03-15 12:37:37 -0300

    Fix ajustando Arraylist para imprimir produtos e preço do ArrayList

--- Modulo 2/Java II Herença/src/Main.java
+++ Modulo 2/Java II Herença/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,6 +10,25 @@ public class Main {
 
         System.out.println(naoPereciveis);
         System.out.println(naoPereciveis.calcular(1));
+
+        NaoPericiveis produtoNP1 = new NaoPericiveis("Enlatado", "Palmito", 13.7);
+        NaoPericiveis produtoNP2 = new NaoPericiveis("Enlatado", "Azeitona", 6.7);
+        NaoPericiveis produtoNP3 = new NaoPericiveis("Enlatado", "Milho", 4.7);
+        NaoPericiveis produtoNP4 = new NaoPericiveis("Enlatado", "Cogumelo", 15.7);
+        NaoPericiveis produtoNP5 = new NaoPericiveis("Tempero", "Paprica Picante", 13.7);
+
+
+        ArrayList<Produtos> pacoteProdutos = new ArrayList<>();
+        pacoteProdutos.add(produtoNP1);
+        pacoteProdutos.add(produtoNP2);
+        pacoteProdutos.add(produtoNP3);
+        pacoteProdutos.add(produtoNP4);
+        pacoteProdutos.add(produtoNP5);
+
+        for(Produtos prod : pacoteProdutos){
+            System.out.println(prod);
+            System.out.println(prod.calcular(1));
+        };
     }
 
 }
